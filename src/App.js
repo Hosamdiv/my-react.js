4958f0e4068938d9f03f140d34ffc5a093a7a88a
hosam <hosamreda@gmail.com>
2024-03-04 23:16:07 -0800

    www

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Good Hosam</h1>
-      <h1>Good Hosam</h1>
+      <h1>Good User</h1>
       <h1>Hello Hosam</h1>
       <h2>React App</h2>
     </div>
